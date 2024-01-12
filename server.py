2fb4ba0941425bcb98ab8ecba41bad20693223be
pblgroupproject <156415686+pblgroupproject@users.noreply.github.com>
2024-01-12 14:59:38 +0530

    Update Flutter server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request 
 
 app = Flask(__name__)
 
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def server():
     return render_template('index.html')
 
+@app.route('/flutter',methods=['GET'])
+def flutterReturn():
+    json = {}
+    inputargs = str(request.args['query'])
+    json["output"] = inputargs
+    return jsonify(json)
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
