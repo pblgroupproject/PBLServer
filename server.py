9817f379521307fc4d43e6e58e08cff63dd8f040
pblgroupproject <156415686+pblgroupproject@users.noreply.github.com>
2024-01-16 12:53:12 +0530

    Attempt to remove 500 error
    changed uploads function added 2 functions which checks for creation of folder and image to folder

--- server.py
+++ server.py
@@ -8,6 +8,21 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+app.config['UPLOAD_FOLDER'] = './uploads'
+allowed_extensions = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+# Function to check allowed file types
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+# Create the uploads directory if it doesn't exist
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+
 @app.route('/')
 def server():
     return render_template('index.html')
@@ -24,12 +39,17 @@ def upload_file():
     if 'image' not in request.files:
         return jsonify({"error": "No image part"}), 400
     file = request.files['image']
+
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
-    if file:
+
+    if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return jsonify({"message": "File uploaded successfully"}), 200
+    else:
+        return jsonify({"error": "File type not permitted"}), 400
+
 
     
 if __name__ == '__main__':
